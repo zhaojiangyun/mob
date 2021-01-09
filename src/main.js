3e273761d08c33a30545f7a5cd17ef82f522ba84
小白 <8447341+zhaojiangyun@user.noreply.gitee.com>
2021-01-09 18:30:51 +0800

    “配置element”

--- src/main.js
+++ src/main.js
@@ -9,6 +9,11 @@ Vue.config.productionTip = false
 import axios from "@/utils/request"
 Vue.prototype.$axios = axios;
 
+// 配置element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
 new Vue({
   router,
   store,
